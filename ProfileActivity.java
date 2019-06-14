70e51ae80e667ceac5ef36325107eca136d6a547
Mohit <Mohitshah>
2019-06-14 15:02:05 +0530

    Profile Activity And Drawable folder

---
+++ ProfileActivity.java
@@ -0,0 +1,24 @@
+package com.brainberry.cashbackapp;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void click_history_view_all(View view) {
+        Toast.makeText(this,"No History Yet",Toast.LENGTH_SHORT).show();
+    }
+
+    public void redeem_history_view_all(View view) {
+        Toast.makeText(this,"No History Yet",Toast.LENGTH_SHORT).show();
+    }
+}
