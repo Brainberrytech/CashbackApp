05ac7a0efa7ec93bf7d1b462b134444709e2daeb
Mohit <Mohitshah>
2019-06-18 11:45:45 +0530

    Updated

---
+++ CashbackApp-Abhinav/HorizontalRecyclerview/app/src/main/java/com/example/horizontalrecyclerview/Refer_and_earn.java
@@ -0,0 +1,13 @@
+package com.example.horizontalrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Refer_and_earn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refer_and_earn);
+    }
+}
